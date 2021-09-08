3e0a0c6975a2373030c8a7d3220bd66c81dce96b
Yilson Mitte <yilson.mitte@gmail.com>
2021-09-08 00:45:18 -0500

    navbar de admin

--- src/app/component/admin-dashboard/admin-dashboard.component.ts
+++ src/app/component/admin-dashboard/admin-dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-
+  showFiller = false;
   constructor() { }
 
   ngOnInit(): void {
